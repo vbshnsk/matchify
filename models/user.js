58b5032f2294e26c7d2fa4c777deb87bc28da898
vbshnsk <vlad.bushinskiy@gmail.com>
2020-04-12 01:56:04 +0300

    WIP: scrap email regex

--- models/user.js
+++ models/user.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcrypt');
 
 const usernameMatcher =  /^[a-z0-9_-]{3,16}$/;
 const passwordMatcher = /^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/;
-const emailMatcher = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const dateMatcher = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
 
 const hashPassword = password => {
@@ -17,13 +16,12 @@ const hashPassword = password => {
 const validate = async data => {
     if (usernameMatcher.test(data.username) &&
         passwordMatcher.test(data.password) &&
-        emailMatcher.test(data.email) &&
         dateMatcher.test(data.birthdate)) {
             return [data.username, await hashPassword(data.password), data.email, data.birthdate];
     }
     else {
         throw new Error("Registration Error");
-    };
+    }
 }
 
 module.exports = {
