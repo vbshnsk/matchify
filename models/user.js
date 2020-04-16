37438122f229cb8f2a1f4c9639d57b3bde3187d7
vbshnsk <vlad.bushinskiy@gmail.com>
2020-04-16 15:04:20 +0300

    no electricity at home, I decided to add JSDocs bc I can

--- models/user.js
+++ models/user.js
@@ -13,6 +13,11 @@ const hashPassword = password => {
     return hash;
 }
 
+/**
+ *  Validate user data before register.
+ * @param {{username: String, password: String, email: String }} data 
+ */
+
 const validate = data => {
     if (usernameMatcher.test(data.username) &&
         passwordMatcher.test(data.password)) {
@@ -23,6 +28,10 @@ const validate = data => {
     }
 }
 
+/**
+ * Authorization middleware.
+ */
+
 const authorize = () => {
     return async function(ctx, next) {
         const data = ctx.request.body;
@@ -32,12 +41,14 @@ const authorize = () => {
         const passwordValidation = await bcrypt.compare(data.password, user.password);
         if (!passwordValidation) { throw new Error("Authorization Error"); }
 
-        console.log(1);
         ctx.session.userid = user.userid;
 
         await next()
     }
 }
+/**
+ * Registration middleware.
+ */
 
 const register = () => { 
     return async (ctx, next) => {
@@ -48,16 +59,41 @@ const register = () => {
     } 
 }
 
+/**
+ * Logout middleware.
+ */
+
 const logout = () => {
     return async (ctx, next) => {
         ctx.session = null;
         await next();
-    }
+    }       
 }
+
+/**
+ * 
+ * @param {String} userid 
+ * @param  {...String} [columns] 
+ *
+ */
 
 const selectOneByID = async (userid, ...columns) => (await db.query(db.sql`select $raw${columns.length ? columns : '*'} from "User" where userid=${userid}`)).rows[0];
 
+/**
+ * 
+ * @param {String} username 
+ * @param  {...String} [columns] 
+ *  
+ */
+
 const selectOneByUsername = async (username, ...columns) => (await db.query(db.sql`select $raw${columns.length ? columns : '*'} from "User" where username=${username}`)).rows[0];
+
+/**
+ * 
+ * @param {String} id
+ * @param  {{}} data
+ *
+ */
 
 const updateById = async (id, data) => db.query(db.sql`update "User" $set${data} where userid=${id}`);
 
