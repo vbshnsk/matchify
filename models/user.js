20c9e1296d4c5f3f0c06c5b9a3f9130d8c7dc538
vbshnsk <vlad.bushinskiy@gmail.com>
2020-04-20 17:16:57 +0300

    User model to ES6 class

--- models/user.js
+++ models/user.js
@@ -3,21 +3,14 @@
 const db = require('../db');
 const bcrypt = require('bcrypt');
 
-const usernameMatcher =  /^[a-z0-9_-]{3,16}$/;
-const passwordMatcher = /^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/;
-
-const hashPassword = password => {
-    const salt = 10;
-    const hash = bcrypt.hash(password, salt);
-    return hash;
-}
-
 /**
  *  Validate user data before register.
  * @param {{username: String, password: String, email: String }} data 
  */
 
 const validate = data => {
+    const usernameMatcher =  /^[a-z0-9_-]{3,16}$/;
+    const passwordMatcher = /^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/; 
     if (usernameMatcher.test(data.username) &&
         passwordMatcher.test(data.password)) {
             return Object.assign({}, data);
@@ -27,83 +20,123 @@ const validate = data => {
     }
 }
 
-/**
- * Authorization middleware.
- */
+const hashPassword = password => {
+    const salt = 10;
+    const hash = bcrypt.hash(password, salt);
+    return hash;
+}
 
-const authorize = () => {
-    return async function(ctx, next) {
-        const data = ctx.request.body;
-        const user = await selectOneByUsername(data.username);
-        
-        if (user === undefined) { throw new Error("Authorization Error"); }
-        const passwordValidation = await bcrypt.compare(data.password, user.password);
-        if (!passwordValidation) { throw new Error("Authorization Error"); }
+class User {
 
-        ctx.session.userid = user.userid;
-        ctx.session.authorized = true;
-
-        await next()
+    constructor({userid, username, password, email, spotify}){
+        this.userid = userid;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.spotify = spotify;
     }
+
+    /**
+ 	* Authorization middleware.
+ 	*/
+
+    static authorize(){
+        return async function(ctx, next) {
+            const data = ctx.request.body;
+            const user = await User.selectOneByUsername(data.username);
+
+            if (user === undefined) { throw new Error("Authorization Error"); }
+            const passwordValidation = await bcrypt.compare(data.password, user.password);
+            if (!passwordValidation) { throw new Error("Authorization Error"); }
+    
+            ctx.session.userid = user.userid;
+    		ctx.session.authorized = true;
+
+            await next()
+     	  }
+	}
+	
+	/**
+ 	* Registration middleware.
+	 */
+
+    static register(){ 
+        return async (ctx, next) => {
+            const data = validate(ctx.request.body);
+            data.password = await hashPassword(data.password); 
+            await db.query(db.sql.insert("User", data));
+            await next();
+        }      
+    }
+
+    /**
+ 	* Logout middleware.
+ 	*/
+
+    static logout(){
+        return async (ctx, next) => {
+            ctx.session.userid = undefined;
+        	ctx.session.authorized = false;
+            await next();
+        }
+    }
+
+	/**
+ 	* 
+ 	* @param {String} userid 
+ 	* @param  {...String} [columns] 
+ 	*
+ 	*/
+    
+    static async selectOneByID(userid, ...columns) { 
+        try {
+            return new User((await db.query(db.sql`
+            select $raw${columns.length ? columns : '*'} 
+            from "User" 
+            where userid=${userid}`)).rows[0]);
+        } catch (error) {
+            return undefined;
+        }
+    }
+
+    /**
+ 	* 
+	* @param {String} username 
+ 	* @param  {...String} [columns] 
+ 	*  
+ 	*/
+
+    static async selectOneByUsername(username, ...columns) {
+        try {
+            return new User((await db.query(db.sql`
+            select $raw${columns.length ? columns : '*'} 
+            from "User" where 
+            username=${username}`)).rows[0]);
+        } catch (error) {
+            return undefined;
+        }
+    }
+
+    /**
+ 	* 
+ 	* @param {String} id
+ 	* @param  {User} data
+ 	*
+ 	*/
+
+    static async updateById(id, data){
+        try {
+            return new User((await db.query(db.sql`
+            update "User" 
+            $set${data} 
+            where userid=${id} 
+            returning *`)).rows[0]);
+        } catch (error) {
+            return undefined;
+        }
+    }
+
 }
-/**
- * Registration middleware.
- */
 
-const register = () => { 
-    return async (ctx, next) => {
-        const data = validate(ctx.request.body);
-        data.password = await hashPassword(data.password); 
-        await db.query(db.sql.insert("User", data));
-        await next();
-    } 
-}
-
-/**
- * Logout middleware.
- */
-
-const logout = () => {
-    return async (ctx, next) => {
-        ctx.session.userid = undefined;
-        ctx.session.authorized = false;
-        await next();
-    }       
-}
-
-/**
- * 
- * @param {String} userid 
- * @param  {...String} [columns] 
- *
- */
-
-const selectOneByID = async (userid, ...columns) => (await db.query(db.sql`select $raw${columns.length ? columns : '*'} from "User" where userid=${userid}`)).rows[0];
-
-/**
- * 
- * @param {String} username 
- * @param  {...String} [columns] 
- *  
- */
-
-const selectOneByUsername = async (username, ...columns) => (await db.query(db.sql`select $raw${columns.length ? columns : '*'} from "User" where username=${username}`)).rows[0];
-
-/**
- * 
- * @param {String} id
- * @param  {{}} data
- *
- */
-
-const updateById = async (id, data) => db.query(db.sql`update "User" $set${data} where userid=${id}`);
-
-module.exports = {
-    selectOneByUsername,
-    selectOneByID,
-    updateById,
-    register,
-    authorize,
-    logout,
-}
+module.exports = User;
  
