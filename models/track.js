163bb449487cfce7ddff11b8dab96186a08c9e34
vbshnsk <vlad.bushinskiy@gmail.com>
2020-04-23 16:41:03 +0300

    refactor Track to ES6 class

--- models/track.js
+++ models/track.js
@@ -2,40 +2,51 @@
 
 const db = require('../db');
 
-/**
- * 
- * @param {{}} data 
- */
-
-const insert = async (data) => (await db.query(db.sql`insert into "Track" $keys${Object.keys(data)} $values${data} returning trackid`)).rows[0]['trackid'];
-
-/**
- * 
- * @param {String} name 
- * @param {[String]} artists 
- * 
- */
-
-const getID = async (name, artists) => {
-    let res = (await db.query(db.sql`select trackid from "Track" where name=${name} and artists=${artists}`)).rows[0];
-    if(res !== undefined){
-        res = res.trackid;
+class Track {
+    constructor({trackid, name, artists, genres, spotifyid}){
+        this.trackid = trackid;
+        this.name = name;
+        this.artists = artists;
+        this.genres = genres;
+        this.spotifyid = spotifyid;
     }
-    return res;
-};
 
-/**
- * 
- * @param {String} trackid 
- * @param {String} userid 
- * 
- */
+    /**
+    * 
+    * @param {Track} data 
+    * 
+    */
 
-const insertPlay = async (trackid, userid) => db.query(db.sql.insert("Play", {trackid, userid}));
+    static async insert(data){
+        return (await db.query(db.sql`insert into "Track" $keys${Object.keys(data)} $values${data} returning trackid`)).rows[0];
+    }
+
+    /**
+    * 
+    * @param {{}} options 
+    * 
+    */
+
+    static async getID(options){
+        let res = (await db.query(db.sql`select trackid from "Track" $where${options}`)).rows[0];
+        if(res !== undefined){
+            res = res.trackid;
+        }
+        return res;
+    }
+
+    /**
+    * 
+    * @param {String} trackid 
+    * @param {String} userid 
+    * 
+    */
+
+    static async insertPlay(trackid, userid){
+        return db.query(db.sql.insert("Play", {trackid, userid}));
+    };
+}
 
 
-module.exports = {
-    insert,
-    insertPlay,
-    getID,
- }
+
+module.exports = Track;
